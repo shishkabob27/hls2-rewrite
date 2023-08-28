8128c4a797620fb1447b0996a5e8e25e34c19685
Xenthio <thegooster88@gmail.com>
2023-08-29 09:19:35 +1000

    monster_barney

---
+++ code/HalfLife/Monsters/Barney.cs
@@ -0,0 +1,25 @@
+﻿using Editor;
+using Sandbox;
+using System.Collections.Generic;
+using XeNPC2;
+
+namespace HLS2; 
+[Library( "monster_barney" ), HammerEntity]
+[EditorModel( "models/hl1/monster/barney.vmdl" )]
+[Title( "Barney" ), Category( "Monsters" ), Icon( "person" )]
+public class Barney : NPC
+{  
+	public override void Spawn()
+	{
+		base.Spawn();
+		SetModel( "models/hl1/monster/barney.vmdl" );
+		SetupPhysicsFromAABB( PhysicsMotionType.Keyframed, new Vector3( -16, -16, 0 ), new Vector3( 16, 16, 72 ) );
+		Health = 20;
+	}
+
+	public override void Think()
+	{
+		base.Think();
+		TryMove();
+	}
+}
