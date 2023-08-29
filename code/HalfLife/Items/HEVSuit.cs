6ccb63f8a792c87ed74c143010cd95d95652409b
TheBoxTrot <baehardman@gmail.com>
2023-08-29 15:03:02 +0800

    Create HEVSuit.cs

---
+++ code/HalfLife/Items/HEVSuit.cs
@@ -0,0 +1,37 @@
+﻿using Editor;
+using Sandbox;
+
+namespace HLS2;
+
+[Library( "item_suit" )]
+[HammerEntity]
+[Model( Model = "models/hl1/items/suit.vmdl" )]
+[Title( "Hev Suit" ), Category( "items" ), Icon( "theater_comedy" )]
+public partial class HevSuit : ModelEntity
+{
+	[Property]
+	public float StartChargeAmount { get; set; }
+	public Output OnPlayerTouch { get; set; }
+
+	[Property, ResourceType( "sound" )]
+	[Net]
+	public string PickupSound { get; set; } = "sounds/hl1/fvox/bell.sound";
+	public override void Touch( Entity other )
+	{
+		base.Touch( other );
+		if (other is Player player)
+		{
+			player.HasHEV = true;
+			player.Suit = StartChargeAmount;
+			OnPlayerTouch.Fire( other );
+			Sound.FromScreen( To.Single(player),PickupSound );
+		}
+	}
+
+	public override void Spawn()
+	{
+		base.Spawn();
+		EnableTouch = true;
+	}
+
+}
