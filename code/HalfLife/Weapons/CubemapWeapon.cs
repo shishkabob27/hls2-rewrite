6ce431a43720d8b377a6a8736dd611424dd646f2
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2023-08-29 19:03:37 -0500

    Create CubemapWeapon.cs

---
+++ code/HalfLife/Weapons/CubemapWeapon.cs
@@ -0,0 +1,9 @@
+﻿using Editor;
+using Sandbox;
+
+namespace HLS2;
+[Library( "weapon_cubemap" )]
+public class CubemapWeapon : Weapon
+{
+	public override string ViewModelPath => "models/shadertest/envballs.vmdl";
+}
