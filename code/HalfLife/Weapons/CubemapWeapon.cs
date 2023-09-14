ec59297698d4eb81596ed4ac0af5e250f75161b6
TheBoxTrot <baehardman@gmail.com>
2023-09-14 19:17:56 +0800

    Placeable CubeMaps

--- code/HalfLife/Weapons/CubemapWeapon.cs
+++ code/HalfLife/Weapons/CubemapWeapon.cs
@@ -6,4 +6,26 @@ namespace HLS2;
 public class CubemapWeapon : Weapon
 {
 	public override string ViewModelPath => "models/shadertest/envballs.vmdl";
+	bool Mode = false;
+	public override void PrimaryAttack()
+	{
+		Log.Error( "lol" );
+		var a = new ModelEntity();
+		a.SetModel( "models/shadertest/envballs.vmdl" );
+		if (Mode)
+		{
+			a.SetupPhysicsFromSphere( PhysicsMotionType.Dynamic, a.Position, 10f );
+		}
+		
+		a.Position = Owner.AimRay.Position + Owner.AimRay.Forward * 100;
+		
+	}
+	public override void SecondaryAttack()
+	{
+		base.SecondaryAttack();
+		if (Mode)
+		{
+			Mode = false;
+		}else { Mode = true; }
+	}
 }
